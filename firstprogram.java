b8f122e0f2ec18dd4f070f2e45b47a0f2a22efd3
TejuTitare <119855486+TejuTitare@users.noreply.github.com>
2022-12-05 16:22:38 +0530

    my first program

---
+++ firstprogram.java
@@ -0,0 +1,10 @@
+package com.app.logicalprogrambyjava;
+
+public class CountOccuranceOfChar1 {
+
+	static final int MAX_CHAR = 256;
+
+	// driver Code
+	public static void main(String args[]) {
+System.out.println("hi welcome ");
+	}
